23dc03343deb120637b40bcf928bc2413bb65962
EstebanAaron <lr96aron@gmail.com>
2024-10-10 16:31:22 +0100

    1

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="prueba.php" method="get">
+    <p>
+    <input type="text" name="data" placeholder="Introduce el texto del qr">
+    </p>
+    <p>
+    <input type="text" name="label" placeholder="Introduce el label">
+    </p>
+    <button type="submit">Enviar</button>
+  </form>
+  
+</body>
+</html>
